6998332a47084e4aedb23d42ae8ef6c251b2ea98
Alexandre S <alexandre.strevenski@gmail.com>
2017-07-31 17:17:01 -0300

    Interface default method

---
+++ src/ocajp8/interfaces/Main.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        SampleClass sc = new SampleClass();
+        sc.someMethod();
+        sc.someDefaultMethod();
+    }
+}
