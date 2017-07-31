2f4268d189a752091e29f233ad439e1454907808
Alexandre S <alexandre.strevenski@gmail.com>
2017-07-31 17:15:06 -0300

    Q6

---
+++ src/ocajp8/exam3/part2/Q6.java
@@ -0,0 +1,14 @@
+package exam3.part2;
+
+
+public class Q6 {
+    public static void main(String[] args) {
+        long size = 0xB;
+        //int[] ints = new int[size]; //can't compile because its a long variable;
+
+        System.out.println(size);
+
+        //ints[5] = 10;
+        //System.out.println(ints[5]+10);
+    }
+}
